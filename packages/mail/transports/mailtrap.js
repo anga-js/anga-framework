8faf2ded5108780e93328836168974ce7488dd6d
Kevin Isom <kevin.isom@gmail.com>
2019-06-15 17:25:00 +1200

    Anga Mail module

---
+++ packages/mail/transports/mailtrap.js
@@ -0,0 +1,12 @@
+const Nodemailer = require('nodemailer')
+
+const transport = Nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: process.env.MAILTRAP_USER,
+    pass: process.env.MAILTRAP_PASSWORD,
+  },
+})
+
+module.exports = transport
